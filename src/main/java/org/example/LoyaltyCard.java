aa5e49a0094692a8cab98fc13b1d5e3e6d26bd0e
John <ashton.basile@gmail.com>
2023-09-25 12:17:58 +0100

    I added the starter code for the assignment

---
+++ src/main/java/org/example/LoyaltyCard.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class LoyaltyCard {
+    private String barCode;
+    private String vendor;
+
+    public LoyaltyCard(String barCode, String vendor) {
+        this.barCode = barCode;
+        this.vendor = vendor;
+    }
+
+    public String getVendor() {
+        return this.vendor;
+    }
+
+    public String scan() {
+        return this.barCode;
+    }
+}
